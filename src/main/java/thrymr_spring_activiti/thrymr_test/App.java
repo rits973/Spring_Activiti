331e6f9d608c46afed4888f0428429e58411c0dc
Ritesh Singh <ritesh.kumar@thrymr.net>
2016-08-17 17:02:53 +0530

    First commit.

---
+++ src/main/java/thrymr_spring_activiti/thrymr_test/App.java
@@ -0,0 +1,13 @@
+package thrymr_spring_activiti.thrymr_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
